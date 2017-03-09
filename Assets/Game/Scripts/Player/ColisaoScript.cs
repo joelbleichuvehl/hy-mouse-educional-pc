ed12abc9bdccf40e34f811be27969c33a6e2b441
Joel Bleichuvehl <joel@cetesc.com.br>
2017-03-09 14:28:25 -0300

    Inicio fase 01, teste cenario

---
+++ Assets/Game/Scripts/Player/ColisaoScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColisaoScript : MonoBehaviour {
+
+	//public RectTransform panelReload;
+
+	//public GameObject sangue;
+
+	public bool teste;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		testar ();
+	}
+
+	void OnTriggerEnter2D(Collider2D colisao){
+		if(colisao.gameObject.tag.Equals("Player")){
+			teste = true;
+			//Destroy(colisao.gameObject);
+			//Instantiate(sangue, colisao.gameObject.transform.position, colisao.gameObject.transform.rotation);
+			//panelReload.localScale = new Vector3(1 , 1, 1);
+		}
+
+		if(colisao.gameObject.tag.Equals("ProximoLevel")){
+			Application.LoadLevel("Level_02");
+		}
+	}
+
+	public bool testar(){
+		return teste;
+	}
+}
---
+++ Assets/Game/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	Vector3 distance;
+	float posX;
+	float posY;
+	// Use this for initialization
+	void Update(){
+	
+	}
+	void OnMouseDown () {
+		distance = Camera.main.WorldToScreenPoint (transform.position);
+		posX = Input.mousePosition.x - distance.x;
+		posY = Input.mousePosition.y - distance.y;
+	}
+	
+	// Update is called once per frame
+	void OnMouseDrag () {
+		Vector3 curPos = new Vector3 (Input.mousePosition.x  - posX, Input.mousePosition.y  - posY, distance.z);
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint (curPos);
+		transform.position = worldPos;
+	}
+}
